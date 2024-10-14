91c4caff93d50952a82f62117893cb2a1bdc4a8a
mubarak23 <mubarakaminu340@gmail.com>
2024-10-14 14:00:11 +0100

    feat: get balance from multi mint wallet

---
+++ src/commands/balance.rs
@@ -0,0 +1,27 @@
+use std::collections::BTreeMap;
+use anyhow::Result;
+use cdk::mint_url::MintUrl;
+use cdk::nuts::CurrencyUnit;
+use cdk::wallet::multi_mint_wallet::MultiMintWallet;
+use cdk::Amount;
+
+
+pub asyn fn balance(multi_mint_wallet: &MultiMintWallet) -> Result<()> {
+    mint_balances(multi_mint_wallet).await?
+
+    Ok()
+}
+
+pub asyn fn mint_balances (multi_mint_wallet: &MultiMintWallet) -> Result<Vec<MintUrl, Amount>> {
+   
+  let wallets = BTreeMap<MintUrl, Amount> = multi_mint_wallet.get_balances(&CurrencyUnit::Sat).await?;
+
+
+  let mut wallet_vec = Vec::new();
+
+  for (mint_url, amount) in wallets.iter().enumerate(){
+    let mint_url = mint_url.clone();
+    wallet_vec.push((mint_url, *amount))
+  }
+    Ok(wallet_vec)
+}
