280a4891835540b49466169f1b972fef1c0073f0
mubarak23 <mubarakaminu340@gmail.com>
2024-10-15 10:12:35 +0100

    feat: decode token function

---
+++ src/commands/decode_token.rs
@@ -0,0 +1,16 @@
+use std::str::FromStr;
+use cdk::nuts::Token;
+use anyhow::Result;
+use cdk::util::serialize_to_cbor_diag;
+use clap::arg;
+
+#[derive(Args)]
+pub struct DecodeTokenSubCommand {
+  token: String 
+}
+
+pub decode_token(sub_command_args: &DecodeTokenSubCommand) -> Result<()> {
+  let token = Token::from_str(&sub_command_args.token)?;
+  
+  println!("{:}", serialize_to_cbor_diag(&token)?);
+}
